52cdbe05c9fb6ef37b12db6b02a71e3fb3457791
bladefisher69-creator <bladefisher69@gmail.com>
2025-10-18 01:12:12 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,6 +14,9 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
